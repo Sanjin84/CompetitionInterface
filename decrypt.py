14bc74cd8f948b76e1347d2dd9c3cc8e0b250249
DawoodLaiq <86498931+DawoodLaiq@users.noreply.github.com>
2021-09-02 05:03:08 +0500

    added password

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,19 @@
 from cryptography.fernet import Fernet
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+
+def go():
+    name=name_var.get()
+    if name=="BOSSMAN":
+        check()
+        button["state"] = DISABLED
+
 
 def check():
-    filename = askopenfilename()
     key = b'DqbtoOEMKXpTsYjm9FFRJOwOXEfUPn5vmUvlOnsx2MY='
 
     fernet = Fernet(key)
     try:
-        with open(filename, 'r') as enc_file:
+        with open("info.txt", 'r') as enc_file:
             encrypted = enc_file.read()
             val = encrypted.split('\n')
             val.pop()
@@ -33,9 +38,13 @@ root.geometry("1000x600")
 root.resizable(width=False, height=False)
 root.title("DASHBOARD")
 root.configure(bg="gray")
+name_var=StringVar()
 
 
-button = Button(root,text="CHOOSE FILE" ,bg="white",font = "Verdana 15 bold", command = check)
-button.place(rely=0.1,relx=0.35, relwidth=0.3, relheight=0.15)
+ent = Entry(root,textvariable = name_var)
+ent.place(rely=0.1,relx=0.2, relwidth=0.3, relheight=0.1)
+button = Button(root,text="LOGIN" ,bg="white",font = "Verdana 15 bold", command = go)
+button.place(rely=0.1,relx=0.6, relwidth=0.3, relheight=0.1)
+
 
 mainloop()
